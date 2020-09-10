abc911a36d7b7dd5f524d6eb5aa8bf4663c28082
newmediapilot <marcin@newmediapilot.com>
2020-09-09 21:49:24 -0400

    'progress';

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuf7z2MjzJl_TKpJqT4RlAdaWV_IkKJl0",
+  authDomain: "arjs1-6b62c.firebaseapp.com",
+  databaseURL: "https://arjs1-6b62c.firebaseio.com",
+  projectId: "arjs1-6b62c",
+  storageBucket: "arjs1-6b62c.appspot.com",
+  messagingSenderId: "981645478924",
+  appId: "1:981645478924:web:f703d9aa8f784337123229"
+};
