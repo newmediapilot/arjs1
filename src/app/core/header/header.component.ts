c35a45e92c7202c552cfb6adbe05c80c9290414e
newmediapilot <marcin@newmediapilot.com>
2020-09-10 20:20:01 -0400

    add header component

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {take} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  user;
+
+  constructor(
+    private uService: UserService,
+    private aRoute: ActivatedRoute,
+    private router: Router) {
+      this.user = uService.getLocalUser();
+  }
+
+  logout() {
+    this.uService.logout().pipe(take(1))
+      .subscribe(() => {
+        this.router.navigate(['login']);
+      });
+  }
+
+}
