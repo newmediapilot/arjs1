5da65e04bd7ba44f86d4c704856b953071400aea
newmediapilot <marcin@newmediapilot.com>
2020-09-12 02:39:46 -0400

    list items

--- src/app/auth/editor/editor.component.ts
+++ src/app/auth/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {storage} from 'firebase/app';
 import {Observable} from 'rxjs';
@@ -12,31 +12,32 @@ import {AngularFireDatabase} from '@angular/fire/database';
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnDestroy {
+export class EditorComponent implements OnInit {
 
   loading: boolean = false;
+  list;
+  uid = this.userService.uid;
 
   constructor(
     private userService: UserService,
     private db: AngularFireDatabase) {
   }
 
-  ngOnDestroy(): void {
-    //
+  ngOnInit(): void {
+    this.list = this.db.list(`images/${this.uid}`).valueChanges();
   }
 
   saveToDb(data) {
-    return fromPromise(this.db.list(`images/${data.uid}`).push(data));
+    return fromPromise(this.db.list(`images/${this.uid}`).push(data));
   }
 
   saveToFirebase(file: File) {
     return new Observable((observer) => {
 
       let ref = storage().ref();
-      let uid = this.userService.uid;
       let timestamp = new Date().getTime();
       let metadata = {contentType: 'image/png'};
-      let path = `slides/${uid}/${timestamp}`;
+      let path = `slides/${this.uid}/${timestamp}`;
 
       let uploadTask = ref.child(path).put(file, metadata);
 
@@ -46,7 +47,7 @@ export class EditorComponent implements OnDestroy {
         () => uploadTask.snapshot.ref.getDownloadURL().then((url) => {
           observer.next(
             {
-              uid: uid,
+              uid: this.uid,
               createdAt: timestamp,
               path: path,
               url: url
